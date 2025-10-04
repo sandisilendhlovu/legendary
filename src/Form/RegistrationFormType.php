8c114059ae820b1c72d669fc87d8d66f85e4ec02
Sandy <74529700+sandisilendhlovu@users.noreply.github.com>
2025-10-04 23:33:29 +0200

    Update registration form fields, reorder form, and add confidentiality disclaimer

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,18 +19,36 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
+            ->add('firstName', TextType::class, [
+            'label' => 'Name',
+            'constraints' => [
+              new NotBlank ([
+                'message' => 'Please enter your first name',
+              ]),
+            ],
+        ])
+            ->add('lastName', TextType::class, [
+              'label' => 'Surname',
+              'constraints' => [
+                new NotBlank([
+                    'message' => 'Please enter your surname',
+                ]), 
+            ],
+          
+        ])
+
+            ->add('email', EmailType::class, [
+                'label' => 'Afrihost Email Address',
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                    new NotBlank([
+                        'message' => 'Please enter your Afrihost email address'
+                        
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+
+               ->add('plainPassword', PasswordType::class, [
+                'label' => 'Password',
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
@@ -45,6 +65,17 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+
+            ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'I agree to the Terms and Conditions',
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You must agree to the terms and conditions to register',
+                    ]),
+                ],
+            ])
+
         ;
     }
 
