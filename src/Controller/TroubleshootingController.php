d457e05099d148170d675242fa45adee872ebcd9
Sandy <74529700+sandisilendhlovu@users.noreply.github.com>
2025-11-18 09:31:45 +0200

    chore(security): add explicit ROLE_USER protection to troubleshooting controller

--- src/Controller/TroubleshootingController.php
+++ src/Controller/TroubleshootingController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class TroubleshootingController extends AbstractController
 {
     #[Route('/troubleshooting', name: 'app_troubleshooting')]
