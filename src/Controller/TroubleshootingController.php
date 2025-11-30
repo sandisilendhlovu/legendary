bbdada5d578c4f2709fa671d3971b674502ed04f
Sandy <74529700+sandisilendhlovu@users.noreply.github.com>
2025-11-30 02:50:57 +0200

    refactor(troubleshooting): update route names and apply GET-only methods across troubleshooting pages.

--- src/Controller/TroubleshootingController.php
+++ src/Controller/TroubleshootingController.php
@@ -10,55 +10,55 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('ROLE_USER')]
 class TroubleshootingController extends AbstractController
 {
-    #[Route('/troubleshooting', name: 'app_troubleshooting')]
+    #[Route('/troubleshooting', name: 'troubleshooting', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('troubleshooting/index.html.twig');
     }
 
-    #[Route('/troubleshooting/airmobile', name: 'app_troubleshooting_airmobile')]
+    #[Route('/troubleshooting/airmobile', name: 'troubleshooting_airmobile', methods: ['GET'])]
     public function airmobile(): Response
     {
         return $this->render('troubleshooting/airmobile.html.twig');
     }
 
-    #[Route('/troubleshooting/wireless', name: 'app_troubleshooting_wireless')]
+    #[Route('/troubleshooting/wireless', name: 'troubleshooting_wireless', methods: ['GET'])]
     public function wireless(): Response
     {
     return $this->render('troubleshooting/wireless.html.twig');
     }
 
-    #[Route('/troubleshooting/dsl', name: 'app_troubleshooting_dsl')]
+    #[Route('/troubleshooting/dsl', name: 'troubleshooting_dsl', methods: ['GET'])]
      public function dsl(): Response
     {
     return $this->render('troubleshooting/dsl.html.twig');
     }
 
-    #[Route('/troubleshooting/fibre-orders', name: 'app_troubleshooting_fibre_orders')]
+    #[Route('/troubleshooting/fibre-orders', name: 'troubleshooting_fibre_orders', methods: ['GET'])]
     public function fibreOrders(): Response
     {
     return $this->render('troubleshooting/fibre_orders.html.twig');
     }
 
-    #[Route('/troubleshooting/fibre-technical', name: 'app_troubleshooting_fibre_technical')]
+    #[Route('/troubleshooting/fibre-technical', name: 'troubleshooting_fibre_technical', methods: ['GET'])]
     public function fibreTechnical(): Response
    {
     return $this->render('troubleshooting/fibre_technical.html.twig');
    }
 
-   #[Route('/troubleshooting/voip', name: 'app_troubleshooting_voip')]
+   #[Route('/troubleshooting/voip', name: 'troubleshooting_voip', methods: ['GET'])]
     public function voip(): Response
    {
     return $this->render('troubleshooting/voip.html.twig');
    }
 
-   #[Route('/troubleshooting/hosting', name: 'app_troubleshooting_hosting')]
+   #[Route('/troubleshooting/hosting', name: 'troubleshooting_hosting', methods: ['GET'])]
    public function hosting(): Response
    {
     return $this->render('troubleshooting/hosting.html.twig');
    }
 
-   #[Route('/troubleshooting/accounts', name: 'app_troubleshooting_accounts')]
+   #[Route('/troubleshooting/accounts', name: 'troubleshooting_accounts', methods: ['GET'])]
    public function accounts(): Response
   {
     return $this->render('troubleshooting/accounts.html.twig');
