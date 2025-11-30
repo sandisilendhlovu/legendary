8da73a7e0a296f0852a435951665b322bfe99f0f
Sandy <74529700+sandisilendhlovu@users.noreply.github.com>
2025-11-30 03:10:35 +0200

    refactor(home): clean up home route naming and add GET method

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,9 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'home', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
