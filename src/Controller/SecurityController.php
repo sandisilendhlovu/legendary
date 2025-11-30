ee812f78fa1cc7dd8d22bc62a898cdd077c96da1
Sandy <74529700+sandisilendhlovu@users.noreply.github.com>
2025-11-30 02:34:45 +0200

    refactor(auth): tidy up auth route names, methods, and verify-notice route

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,28 +9,28 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/login', name: 'auth_login', methods: ['GET', 'POST'])]
     public function login(AuthenticationUtils $authenticationUtils): Response
-    { 
+    {
 
         // Retrieve the last entered username and any login error
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername, 
-             'error' => $error, 
+            'last_username' => $lastUsername,
+             'error' => $error,
             ]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route(path: '/logout', name: 'auth_logout', methods: ['GET'])]
     public function logout(): void
     {
         // Symfony handles logout automatically based on the firewall configuration
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-    
-   #[Route('/verify-notice', name: 'app_verify_notice')]
+
+   #[Route('/verify-notice', name: 'auth_verify_notice',methods: ['GET'])]
     public function verifyNotice(): Response
     {
         return $this->render('security/verify_notice.html.twig');
