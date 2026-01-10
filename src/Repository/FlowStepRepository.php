6837732ee3f74f29fe574a119663649f54574885
Sandy <74529700+sandisilendhlovu@users.noreply.github.com>
2026-01-11 00:11:01 +0200

    fix: correct Flow entity type hint in FlowStepRepository

--- src/Repository/FlowStepRepository.php
+++ src/Repository/FlowStepRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Flow;
 use App\Entity\FlowStep;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
